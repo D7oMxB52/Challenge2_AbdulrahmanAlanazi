cc89d6a5523af036d24ef1e4ece1b24ca9aed925
D7oMxB52 <s3813024@student.rmit.edu.au>
2022-10-11 02:14:35 +1100

    trying to make successful build

--- accountservice/src/test/java/com/accountservice/accountservice/AccountserviceApplicationTests.java
+++ accountservice/src/test/java/com/accountservice/accountservice/AccountserviceApplicationTests.java
@@ -3,7 +3,7 @@ package com.accountservice.accountservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AccountserviceApplicationTests.class)
 class AccountserviceApplicationTests {
 
     @Test
--- personservice/src/test/java/com/personservice/personservice/PersonserviceApplicationTests.java
+++ personservice/src/test/java/com/personservice/personservice/PersonserviceApplicationTests.java
@@ -3,7 +3,7 @@ package com.personservice.personservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PersonserviceApplicationTests.class)
 class PersonserviceApplicationTests {
 
     @Test
